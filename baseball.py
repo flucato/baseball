0c3a7e958b525b78aef34f8ed4688eeaacf7a4c6
flucato@gmail.com <flucato@gmail.com>
2019-04-01 13:29:23 -0500

    New update , added new list for generator/random

--- baseball.py
+++ baseball.py
@@ -17,52 +17,51 @@ def baseball():
 
 dictionary = {
 
-"1, 1" : "double - 2",
-"1, 2" : "single - 1",
-"1, 3" : "single - 1",
-"1, 4" : "single -1",
-"1, 5" : "base on error - 1",
-"1, 6" : "base on balls -1",
-"2, 2" : "strike - 1",
-"2, 3" : "strike - 1",
-"2, 4" : "strike - 1",
-"2, 5" : "strike - 1",
-"2, 6" : "foul out", 
-"3, 3" : "out at 1st",
-"3, 4" : "out at 1st",
-"3, 5" : "out at 1st",
-"3, 6" : "out at 1st",
-"4, 4" : "fly out",
-"4, 5" : "fly out",
-"4, 6" : "fly out",
-"5, 5" : "double play",
-"5, 6" : "triple",
-"6, 6" : "home run",
+"1,1" : "double - 2",
+"1,2" : "single - 1",
+"1,3" : "single - 1",
+"1,4" : "single -1",
+"1,5" : "base on error - 1",
+"1,6" : "base on balls -1",
+"2,2" : "strike - 1",
+"2,3" : "strike - 1",
+"2,4" : "strike - 1",
+"2,5" : "strike - 1",
+"2,6" : "foul out", 
+"3,3" : "out at 1st",
+"3,4" : "out at 1st",
+"3,5" : "out at 1st",
+"3,6" : "out at 1st",
+"4,4" : "fly out",
+"4,5" : "fly out",
+"4,6" : "fly out",
+"5,5" : "double play",
+"5,6" : "triple",
+"6,6" : "home run",
 
 }
 
-numbers = [11,12,13,14,15,16,22,23,24,25,26,33,34,35,36,44,45,46,55,56,66]
+numbers = ['1,1','1,2','1,3','1,4','1,5','1,6','2,2','2,3','2,4','2,5','2,6','3,3','3,4','3,5','3,6','4,4','4,5','4,6','5,5','5,6','6,6']
 
 r = random.choice(numbers)
 print(r)
 
-dic = 
+step = dictionary[r]
+print(step)
+
+
+
+
 
 
 
 """
+#test to determine dice rolls using random 
 die_1 = random.randint(1,6)
 die_2 = random.randint(1,6)
 print(die_1)
 print(die_2)
 """
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     baseball()
